87c91ca7cf95af3f0265fcf7f971b400aa074ae6
Peter Fox <peter.fox@peterfox.me>
2019-03-02 12:53:09 +0000

    Adds a Make command with a generated class

---
+++ app/Console/Commands/Make/MakeClassCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands\Make;
+
+use Illuminate\Console\GeneratorCommand;
+
+class MakeClassCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:class';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new a class';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'class';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return resource_path('stubs/ClassStub.php');
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param string $rootNamespace
+     *
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Make a Command Demo
+
+This is the code for the article [Laravel: Making make commands](https://medium.com/@SlyFireFox/laravel-making-make-commands-98b7584dbc01)
+that demos how to make a new Make command by [Peter Fox](https://twitter.com/SlyFireFox)
